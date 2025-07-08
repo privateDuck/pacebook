12093f10da947f660328f344bf6c6528fa968580
sanjanadissa <sddsdonz@gmail.com>
2025-07-09 02:03:21 +0530

    update the basic home feed

--- src/main/java/com/groupd/pacebook/controller/PostController.java
+++ src/main/java/com/groupd/pacebook/controller/PostController.java
@@ -1,4 +1,28 @@
 package com.groupd.pacebook.controller;
 
+import com.groupd.pacebook.model.Post;
+import com.groupd.pacebook.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/posts")
 public class PostController {
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public String createPost(@ModelAttribute Post postDto, Principal principal) {
+        postService.savePost(postDto.getContent(), principal.getName());
+        return "redirect:/home";
+    }
 }
